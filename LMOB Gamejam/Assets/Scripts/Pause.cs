21ee9d3a8bd80fcf4f71e5c1661489d971ad7d07
trannu <taavirannu99@gmail.com>
2021-03-15 23:13:56 +0200

    Day 1

---
+++ LMOB Gamejam/Assets/Scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject player;
+    public Canvas my_canvas;
+
+    private void Start()
+    {
+        //my_canvas.enabled = false;
+    }
+
+    public void OnTriggerEnter2D()
+    {
+        Debug.Log("k");
+        my_canvas.enabled = true;
+    }
+
+}
