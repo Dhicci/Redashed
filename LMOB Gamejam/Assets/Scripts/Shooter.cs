85dd5d0bba7762613cc320088a2629f00df8e7fb
trannu <taavirannu99@gmail.com>
2021-03-16 13:25:34 +0200

    Before Time

---
+++ LMOB Gamejam/Assets/Scripts/Shooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bullet_prefab;
+    public Transform fire_point;
+    public float shoot_delay = 3.0f;
+    float time_left;
+
+    private void Start()
+    {
+        time_left = shoot_delay;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        time_left -= Time.deltaTime;
+        if (time_left <= 0)
+        {
+            time_left = shoot_delay;
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet_prefab, fire_point.position, fire_point.rotation);
+    }
+}
---
+++ LMOB Gamejam/Assets/Scripts/TriggerKill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerKill : MonoBehaviour
+{
+
+    public GameObject game_manager;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Kill player
+        if (collision.gameObject.tag == "Player")
+        {
+            game_manager.GetComponent<GameManager>().PlayerDeath();
+        }
+    }
+
+}
