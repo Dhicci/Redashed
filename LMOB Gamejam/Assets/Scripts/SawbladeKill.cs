bbd884b4cc51d05a65295303d46caba601b97c1e
trannu <taavirannu99@gmail.com>
2021-03-17 23:46:29 +0200

    sawblades

---
+++ LMOB Gamejam/Assets/Scripts/SawbladeKill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawbladeKill : MonoBehaviour
+{
+
+    GameObject game_manager;
+
+    private void Start()
+    {
+        game_manager = GameObject.FindGameObjectWithTag("GameManager");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Kill player
+        if (collision.gameObject.tag == "Player")
+        {
+            if (game_manager.GetComponent<GameManager>().rewind == false)
+            {
+                game_manager.GetComponent<GameManager>().PlayerDeath();
+            }
+        }
+    }
+}
