3a4f87964e02bd2aed72865a89d51d8c55b79f00
trannu <taavirannu99@gmail.com>
2021-03-18 02:13:22 +0200

    Menu added

---
+++ LMOB Gamejam/Assets/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transition_time = 1f;
+    
+    public void LoadNextLevel(int x)
+    {
+        StartCoroutine(LoadLevel(x));
+    }
+
+    IEnumerator LoadLevel(int level_index)
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(transition_time);
+
+        SceneManager.LoadScene(level_index);
+    }
+}
